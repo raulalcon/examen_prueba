609b898776fe6036d2f4983f9b3b4445b3807649
raulalcon <raul.alcjim@gmail.com>
2021-11-17 10:26:51 +0100

    commit

---
+++ insertar_tapa.php
@@ -0,0 +1,66 @@
+<?php
+
+include 'head.php';
+session_start();
+if(isset($_REQUEST['insertar'];))
+{
+  $codigo_tapa=$_REQUEST['codigo'];
+  $nombre_tapa=$_REQUEST['nombre'];
+  $precio_tapa=$_REQUEST['precio'];
+  $tipo_tapa=$_REQUEST['tipo'];
+  $_SESSION['tapas'][$codigo_tapa]=array ($codigo_tapa, $nombre_tapa, $precio_tapa, $tipo_tapa;)
+}
+print' 
+        <h2 class="postheader">FORMULARIO PARA AÑADIR UNA TAPA</h2>
+                                     
+           <div   class="postcontent">
+                <form action="" method="post">
+                    <table align="center" class="content-layout">
+                     <tr>
+                      <td align="right"><strong>Codigo de la Tapa :</strong></td>
+                      <td>
+                        <input type="text" name="codigo" size="10" />
+                      </td>
+                     </tr>
+                     <tr>
+                      <td align="right"><strong>Nombre de la Tapa :</strong></td>
+                      <td>
+                        <input type="text" name="nombre" size="70" />
+                      </td>
+                     </tr>
+                     <tr>
+                      <td align="right"><strong>Precio :</strong></td>
+                      <td>
+                        <input type="text" name="precio" value="0" size="7" />€
+                      </td>
+                     </tr>
+                     
+                     <tr>
+                        <td align="right"><strong>Tipo de Tapa :</strong></td>
+                        <td>
+                          <div align="left">
+                                <select name="tipo_tapa">
+                                  <option value="0">Fria</option>
+                                  <option value="1">Caliente</option>
+                                  <option value="2">Bocadillo</option>
+                                  
+                                </select>
+                           </div>
+                          </td>
+                    </tr>
+                   
+                    <tr>
+                        <td colspan="2">
+                          <div align="center"><strong>
+                            <input name="calcular" type="submit" value="Insertar Tapa"/>
+                            </strong>
+                          </div>
+                        </td>
+                    </tr>
+                    </table>
+        </form>
+        </div>';
+
+
+include 'pie.php';
+
---
+++ listado_votos.php
@@ -0,0 +1,8 @@
+<?php
+
+include 'head.php';
+session_start();
+var_dump($_SESSION['tapas'])                                       
+ 
+ include 'pie.php';
+
